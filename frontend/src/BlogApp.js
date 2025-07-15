b62805983086ffe33c6fce9867d2cd03a9c308e3
monica-raluca <raluca.birladeanu@stud.acs.upb.ro>
2025-07-15 17:19:47 +0300

    react setup

---
+++ frontend/src/BlogApp.js
@@ -0,0 +1,8 @@
+export function BlogApp() {
+  return (
+    // <div>
+    //   Hello, world!
+    // </div>
+    <button>This is a shiny button!</button>
+    );
+}
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,1 +1,19 @@
-console.log('Hello world!');
+import { createRoot } from 'react-dom/client';
+import { BlogApp} from './BlogApp';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<BlogApp/>);
+
+// // console.log('Hello world!');
+// import { createRoot } from 'react-dom/client';
+
+// // Clear the existing HTML content
+// document.body.innerHTML = '<div id="app"></div>';
+
+// // Render your React component instead
+// const root = createRoot(document.getElementById('app'));
+// root.render(<h1>Hello, world</h1>);
